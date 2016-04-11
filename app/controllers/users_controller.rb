0d63d5afeee942e00b0cdba9ef5e4f25a21a21c6
David Fisher <davie.fisher@gmail.com>
2016-04-11 10:31:43 -0700

    authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id.to_s
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :gender, :location, :email, :password, :password_confirmation)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
+
+  root 'users#new'
+
+  get 'users' => 'users#index'
+
+  get 'users/new' => 'users#new'
+
+  post 'users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+
+  post '/login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
